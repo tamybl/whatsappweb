ad999076902b1e018e265bbff0d0e0daf2259173
tamybl <tamara.bravo.l@gmail.com>
2017-12-19 12:28:27 -0300

    Correcciones de estilo y funcionalidad input nuevo comentario

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,36 +6,35 @@ $(document).ready(function () {
 		var photo = $(this).find('.photo-profile').html();
 		var lastconection = $(this).find('.lastdate').text();
 		var messageFriend = $(this).find('.lastmessage').text();
-		$('#friend-panel').html('<div class="row friend-full"><div class="col-md-2"><div class="photo-profile">'+photo+'</div></div><div class="col-md-10 pad-x"><div class="name">'+name+'</div><div class="lastconection"> Ultima conexión:' +lastconection+'</div></div></div>');
+		$('#box-messages').html('<div class="row margin-0"><header class="col-md-12 friend-panel" id="friend-panel"><div class="row friend-full"><div class="col-md-2"><div class="photo-profile">'+photo+'</div></div><div class="col-md-10 pad-x"><div class="name">'+name+'</div><div class="lastconection"> Ultima conexión:'+lastconection+'</div></div></div></header><div class="col-md-12 messages" id="messages"><div class="box-submessage" id="submessages"><p>	Mensaje de ejemplo</p></div></div><div class="col-md-12 send-messages" ><input type="text" id="text-send" placeholder="Escribe tu mensaje aquí"><button id="send-btn"><i class="fa fa-microphone fa-lg" aria-hidden="true"></i></button></div></div>');
 		// Conversacion 
-		$('#submessages').html('<span><img src="assets/img/icon-chat.png"<span><div class="inl-block"><p>'+ messageFriend+'</p><p class="hour">'+lastconection+'</p><div>');
-	});
+		$('#submessages').html('<div class="inl-block"><p>'+ messageFriend+'</p><p class="hour">'+lastconection+'</p><div>');
 
-	$('#send-btn').click( function () {
-		if ($('#text-send').val() !== '') {
-			sendMessage(); 
-		}
-		else {
-			alert('Ingresa un texto');
-		}
-	});
-
-	$('#text-send').keyup( function (e) {
-		if ($('#text-send').val() !== '') {
-			if(e.keyCode == 13) {
-				sendMessage();
+		$('#send-btn').on("click", function () {
+			if ($('#text-send').val() !== '') {
+				sendMessage(); 
 			}
-		}
-		else {
-			alert('Ingresa un texto');
-		}
-		
-	})
+		});
+
+		$('#text-send').on("keyup keypress", function (e) {
+			if ($('#text-send').val() !== '') {
+				$('#send-btn').html('<i class="fa fa-paper-plane fa-lg" aria-hidden="true"></i>');
+				if(e.keyCode == 13) {
+					sendMessage();
+				}
+			}
+			else {
+				$('#send-btn').html('<i class="fa fa-microphone fa-lg" aria-hidden="true"></i>');
+				//alert('Ingresa un texto');
+			}	
+		})
+	});
 
 	function sendMessage () {
 		var newMessage = $('#text-send').val();
 			$('#messages').append('<div class="mymessage"><p>'+newMessage+'</p><p class="hour">'+myDate()+'</p></div>');
-			//$('#text-send').empty();
+			$('#text-send').val('');
+			$('#send-btn').html('<i class="fa fa-microphone fa-lg" aria-hidden="true"></i>');
 	}
 
 	function myDate () {
